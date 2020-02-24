9e0f74b21f24a2313e927e69f37004ba410044d1
sonu_kumar_singh <sonu_kumar_singh@external.mckinsey.com>
2020-02-24 17:58:40 +0530

    test cases added

--- hackernews/links/tests.py
+++ hackernews/links/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import RequestFactory, TestCase
 
-# Create your tests here. test cases will be here
+
+class SimpleTest(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        print('Test cases here')
+
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
